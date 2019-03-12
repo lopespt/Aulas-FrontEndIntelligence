e28b37f1d12739793f416bac56b80ffdfe2b728c
lopespt <guilhermewachs@gmail.com>
2019-03-12 19:10:32 -0300

    Aula 6 - Vue

---
+++ Aula6/exemplos/ex10/index.js
@@ -0,0 +1,12 @@
+import Vue from "vue/dist/vue.esm"
+import App from "./app.vue"
+
+let app = new Vue({
+    el: "#app",
+    components: {
+        App
+    },
+    template: "<App></App>"
+});
+
+
---
+++ Aula6/exemplos/ex9/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue/dist/vue.esm"
+import App from "./app.vue"
+
+
+let app = new Vue({
+    el: "#app",
+    components: {
+        App
+    },
+    template: "<App></App>"
+});
+
+
