0bae8422a8c0815174733011a68beffb7c9b3a33
lopespt <guilhermewachs@gmail.com>
2019-03-26 17:17:04 -0300

    Aula 6

---
+++ Aula6/exemplos/ex0/meu_site/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Teste texto="abc">
+      <div>opa</div>
+      </Teste>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Teste from './components/Teste.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Teste
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Aula6/exemplos/ex0/meu_site/src/components/Teste.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-on:click="opa">
+        {{a}}
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {a: 1}
+    },
+    props:{
+        'texto': String
+    },
+    methods:{
+        opa(){
+            this.a = 9;
+            alert("teste");
+        }
+    }
+}
+</script>
+
+<style scoped>
+div{
+    background-color: #aaa
+}
+</style>
